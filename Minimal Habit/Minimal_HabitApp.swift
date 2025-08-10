63aa50c3c7895e73cbd32017a5a9a889a4c93ca1
Tunahan <tunahan.ata@gmail.com>
2025-08-11 01:17:48 +0200

    First Bench

---
+++ Minimal Habit/Minimal_HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimal_HabitApp.swift
+//  Minimal Habit
+//
+//  Created by Tunahan Ata on 11.08.25.
+//
+
+import SwiftUI
+
+@main
+struct Minimal_HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
